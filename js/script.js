3ba7883302c2088df7833139ae48d1ec52312f8a
snow-one <123709771+snow-one@users.noreply.github.com>
2023-02-01 18:51:24 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================== typing animation ======================== */
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer"," Graphic Desigenr"," Youtuber"],
+    tyeSpeed:100,
+    BachSpeed:60,
+    loop:true
+})
